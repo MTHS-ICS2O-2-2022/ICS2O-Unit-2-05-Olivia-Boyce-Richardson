a9dbd0c5f5eaf7addc4776df8b5eec2a19c66a3c
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-03-30 16:30:03 +0000

    html fixes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This file contains the JS functions for index.html
+function enterClicked() {
+  // input
+  const pay = document.getElementById("pay").value
+  const hours = document.getElementById("hours").value
+
+  // output
+  document.getElementById('pay').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+  document.getElementById('pay').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
+}
