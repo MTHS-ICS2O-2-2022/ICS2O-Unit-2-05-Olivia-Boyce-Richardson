fb46e29d76f0a2ee1c86cf738ee185060297073e
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-05 15:26:42 +0000

    html fixes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-function enterClicked() {
+'use strict'
+/**
+ * This function calculates your pay depending on the amount of hours you work (including tax).
+ */
+function calculate () {
   // input
-  const payPerHour = parseInt.getElementById("pay-per-hour").value
-  const hoursWorked = parseInt.getElementById("hours-worked").value
+  const pay = parseInt(document.getElementById('pay-per-hour').value)
+  const hours_worked = parseInt(document.getElementById('hours-worked').value)
 
-  // process 
-  const totalSalary = payPerHour * hoursWorked
-  const governmentTakes = (payPerHour * hoursWorked) * 0.18
+  // process
+  const net_payment = (hours_worked * pay) * (1.00 - 0.18)
+  const taxes = (hours_worked * pay) *  0.18
 
   // output
-  document.getElementById("total-salary").innerHTML = "Your pay will be: " totalSalary "."
-  document.getElementById('government-takes').innerHTML = 'The government will take ' governmentTakes '.'
+  document.getElementById('net-payment').innerHTML = `Your pay will be: $ ${net_payment.toFixed(2)}`
+  document.getElementById('tax').innerHTML = `the government will take: $ ${taxes.toFixed(2)}`
 }
